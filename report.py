0bb29bc12c5bb7099fc241f3eff24b374f4e44fc
Tahy1 <33107612+Tahy1@users.noreply.github.com>
2019-10-10 11:18:52 +0800

    Update report.py

--- report.py
+++ report.py
@@ -10,8 +10,8 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.metrics import roc_auc_score
 
-probs = torch.load('maxs-1.pth')
-targets = torch.load('test.lib')['targets']
+probs = torch.load('probs.pth')
+targets = torch.load('test-100.lib')['targets']
 preds = [1 if i>0.5 else 0 for i in probs]
 print(confusion_matrix(targets, preds))
 print(classification_report(targets, preds, digits=4))
